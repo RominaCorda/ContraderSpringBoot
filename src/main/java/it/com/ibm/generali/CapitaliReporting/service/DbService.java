1e1fb9ab535316b925731c1d2dbef188dbff855e
guildenstern <alessiosaltarin@gmail.com>
2017-08-29 11:57:31 +0200

    Added users db

--- src/main/java/it/com/ibm/generali/CapitaliReporting/service/DbService.java
+++ src/main/java/it/com/ibm/generali/CapitaliReporting/service/DbService.java
@@ -1,12 +1,16 @@
 package it.com.ibm.generali.CapitaliReporting.service;
 
 
+import it.com.ibm.generali.CapitaliReporting.config.Users;
 import it.com.ibm.generali.CapitaliReporting.dao.UserDao;
 import it.com.ibm.generali.CapitaliReporting.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DbService
@@ -25,11 +29,17 @@ public class DbService
     {
         this.logger.info("Initializing user database...");
 
-        User admin = new User("admin", "pass");
-        User alessio = new User("alessio", "doctor");
+        HashMap<String, String> users = Users.INSTANCE.getGet();
 
-        this.userDao.save(admin);
-        this.userDao.save(alessio);
+        for (Map.Entry<String, String> user : users.entrySet())
+        {
+            String username = user.getKey();
+            String password = user.getValue();
+            this.logger.info("... adding user " + username);
+            User tmpUser = new User(username, password);
+            this.userDao.save(tmpUser);
+        }
+
 
         this.logger.info("Initializing user database done.");
     }
---
+++ src/main/kotlin/it/com/ibm/generali/CapitaliReporting/config/Users.kt
@@ -0,0 +1,16 @@
+package it.com.ibm.generali.CapitaliReporting.config
+
+import java.util.*
+
+object Users
+{
+    val get: HashMap<String, String> =
+            hashMapOf(
+                    "admin" to "pass",
+                    "alessio" to "doctor",
+                    "gian" to "pass",
+                    "lorenzo" to "pass",
+                    "michela" to "pass"
+            )
+
+}
