84d103b7de0b8365976f2b67e8f101c6363d24e1
Gianmaria Borgonovo <gianmaria.borgonovo@it.ibm.com>
2017-10-17 09:05:39 +0200

    Merge branch 'master' of git.ng.bluemix.net:alessiosaltarin/GeneraliApp
    # Conflicts:
    #	src/main/java/it/com/ibm/generali/capitalreporting/service/ReportAvailability.java

---
+++ src/main/java/it/com/ibm/generali/capitalreporting/dto/ReportAvRespDTO.java
@@ -0,0 +1,27 @@
+package it.com.ibm.generali.capitalreporting.dto;
+
+public class ReportAvRespDTO
+{
+    private int status;
+    private String message;
+
+    public int getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(int status)
+    {
+        this.status = status;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+}
