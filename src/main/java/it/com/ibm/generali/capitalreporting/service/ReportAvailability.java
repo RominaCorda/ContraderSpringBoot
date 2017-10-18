239dd06de1a9c52a7ff659af26608b0e23eae57f
Gianmaria Borgonovo <gianmaria.borgonovo@it.ibm.com>
2017-10-18 16:45:47 +0200

    Merge branch 'master' of git.ng.bluemix.net:alessiosaltarin/GeneraliApp
    # Conflicts:
    #	src/main/java/it/com/ibm/generali/capitalreporting/service/ReportAvailability.java

--- src/main/java/it/com/ibm/generali/capitalreporting/service/ReportAvailability.java
+++ src/main/java/it/com/ibm/generali/capitalreporting/service/ReportAvailability.java
@@ -2,13 +2,23 @@ package it.com.ibm.generali.capitalreporting.service;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ReportAvailability
 {
-
     public String reportFound(String environmentID, String reportID, String reportFile)
     {
-        return environmentID + "_" + reportID + "_" + reportFile;
+        // Check reportFile extension
+        FileService fileService = new FileService();
+        boolean checkExtensionBool = fileService.checkExtension(reportFile);
+
+        /*
+         * Temporary implementation
+         */
+        String X = (checkExtensionBool) ? "ZIPFILE" : "OTHERFILE";
+
+        return X + "_" + environmentID + "_" + reportID + "_" + reportFile;
     }
+
 
 }
