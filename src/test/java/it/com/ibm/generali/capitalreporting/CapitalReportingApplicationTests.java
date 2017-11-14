3ff5a71f282697daf34c6299a02c7542b3eba472
guildenstern <alessiosaltarin@gmail.com>
2017-11-14 19:23:43 +0100

    Test fix

--- src/test/java/it/com/ibm/generali/capitalreporting/CapitalReportingApplicationTests.java
+++ src/test/java/it/com/ibm/generali/capitalreporting/CapitalReportingApplicationTests.java
@@ -1,32 +1,18 @@
 package it.com.ibm.generali.capitalreporting;
 
-import it.com.ibm.generali.capitalreporting.service.NumberCruncher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CapitalReportingApplicationTests
 {
-    @Autowired
-    private NumberCruncher numberCruncher;
 
     @Test
     public void contextLoadsTest()
     {
-    }
-
-    @Test
-    public void squareTest()
-    {
-        int square = this.numberCruncher.square(2);
-        assertThat(square, is(4));
     }
 
 }
